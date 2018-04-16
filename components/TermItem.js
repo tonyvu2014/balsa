aeb8722e61e3191eb51bea0600387ff9134c45ea
Tony Vu <tonyvu2008@gmail.com>
2018-04-16 22:48:02 +1000

    Update TermItem component

--- components/TermItem.js
+++ components/TermItem.js
@@ -6,8 +6,12 @@ export default class TermItem extends React.Component {
     render() {
         return (
             <View style={styles.container}>
-                <Text style={styles.term}>{this.props.term}</Text>
-                <Button title='x' color='#000' onPress={() => {Alert.alert('Deleting this item')}}></Button>
+                <View style={styles.content}>
+                    <Text style={styles.term}>{this.props.term}</Text>
+                </View>
+                <View style={styles.action}>
+                    <Button title='x' color='#5cb85c' onPress={() => {Alert.alert('Deleting this item')}}></Button>
+                </View>
             </View>
         )
     }
@@ -16,14 +20,28 @@ export default class TermItem extends React.Component {
 
 const styles = StyleSheet.create({
     container: {
+        flexDirection: 'row',
+        flex: 10,
+        backgroundColor: '#5cb85c',
+        marginTop: 5,
+        marginBottom: 5,
+        paddingTop: 3,
+        paddingBottom: 3
+    },
+
+    content: {
         flexDirection: 'column',
-        flex: 1,
         justifyContent: 'center',
-        backgroundColor: '#f9f9f9'
+        flex: 8,
+        paddingLeft: 3
     },
 
     term: {
-        color: '#000',
+        color: '#fff',
         fontSize: 14
+    },
+
+    action: {
+        flex: 2
     }
 })
